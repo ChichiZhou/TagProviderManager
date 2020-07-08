eb8a0c198a31b28524424fcfc382e164970dc630
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-07-08 15:53:23 -0500

    Add more comments

--- managed-tag-provider-gateway/src/main/java/com/inductiveautomation/ignition/examples/mtp/ManagedProviderGatewayHook.java
+++ managed-tag-provider-gateway/src/main/java/com/inductiveautomation/ignition/examples/mtp/ManagedProviderGatewayHook.java
@@ -24,6 +24,13 @@ import org.apache.log4j.Logger;
  * <p/>
  * There is a "control" tag that can be used to modify the number of tags provided. This tag illustrates how to set up
  * write handling.
+ *
+ * 能否根据这个来模拟一个 tag 流。
+ * 从最初始的状态到把 metric publish 到 cloudwatch， 然后创建 carnaval 之类的
+ *
+ * 这里会创建一个 control tag，其作用是控制所创建的 Custom Tags 的数量。相当于一个开关。是可读可写的
+ * 并且会创建许多别的 tags，其命名格式为 Custom Tags/Tag %d。 这些 tags 会自动更新。但是只读的
+ *
  */
 public class ManagedProviderGatewayHook extends AbstractGatewayModuleHook {
     private static final String TASK_NAME = "UpdateSampleValues";
@@ -54,6 +61,7 @@ public class ManagedProviderGatewayHook extends AbstractGatewayModuleHook {
             configuration.setPersistValues(false);
             configuration.setAttribute(TagProviderMeta.FLAG_HAS_OPCBROWSE, false);
 
+            // 创建需要的 tag provider
             ourProvider = context.getTagManager().getOrCreateManagedProvider(configuration);
             //Set up the control tag.
             //1) Create the tag, and set its type.
@@ -78,6 +86,8 @@ public class ManagedProviderGatewayHook extends AbstractGatewayModuleHook {
     public void startup(LicenseState activationState) {
         try {
             //Register a task with the execution system to update values every second.
+            // 相当于让系统自己跑一个脚本，执行一个程序。而这里执行的是 updateValues，即更新数据
+            // 这里的 1000 应该是指 1000 ms
             context.getExecutionManager().register(getClass().getName(), TASK_NAME, this::updateValues, 1000);
 
             logger.info("Example Provider module started.");
@@ -113,10 +123,12 @@ public class ManagedProviderGatewayHook extends AbstractGatewayModuleHook {
     private synchronized void adjustTags(int newCount) {
         if (newCount > currentTagCount) {
             for (int i = currentTagCount; i < newCount; i++) {
+                // 创建 Tags
                 ourProvider.configureTag(String.format(TAG_NAME_PATTERN, i), DataType.Float8);
             }
         } else if (newCount < currentTagCount) {
             for (int i = currentTagCount; i > newCount; i--) {
+                // 创建 Tags
                 ourProvider.removeTag(String.format(TAG_NAME_PATTERN, i));
             }
         }
@@ -128,10 +140,12 @@ public class ManagedProviderGatewayHook extends AbstractGatewayModuleHook {
 
     /**
      * Update the values of the tags.
+     * 这个作用是每秒钟更新 tag value
      */
     private synchronized void updateValues() {
         Random r = new Random();
         for (int i = 0; i < currentTagCount; i++) {
+            // 更新 Tags
             ourProvider.updateValue(String.format(TAG_NAME_PATTERN, i), r.nextFloat(), QualityCode.Good);
         }
     }
